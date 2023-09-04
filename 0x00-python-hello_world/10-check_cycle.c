29c7d0b50771d51703318a233c1c05b1ca9fc08c
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-04 21:02:10 +0100

    added task 10 - some fixes

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,17 +12,18 @@
 int check_cycle(listint_t *list)
 {
 	const listint_t *head = list;
-	listint_t *current = list->next;
 
 	if (!list)
 		return (0);
 
-	while (current)
+	list = list->next;
+
+	while (list)
 	{
-		if (current == head)
+		if (list == head)
 			return (1);
 
-		current = current->next;
+		list = list->next;
 	}
 
 	return (0);
