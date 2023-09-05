5dd36c44d5cda67d8218d5ca74f32fca0e7a5d3a
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-05 23:52:59 +0100

    empty list fix

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -24,7 +24,10 @@ listint_t *insert_node(listint_t **head, int number)
 	new->next = NULL;
 
 	if (*head == NULL)
+	{
 		*head = new;
+		return (new);
+	}
 	else if (current->n >= number)
 	{
 		new->next = *head;
