b7dfc672e28bacd0a04b24a95e7975510dd54303
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-05 17:31:49 +0100

    added task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def print_last_digit(number):
+    last_digit = number % 10
+    if number < 0:
+        last_digit = 10 - last_digit
+
+    print(last_digit, end="")
+    return(last_digit)
