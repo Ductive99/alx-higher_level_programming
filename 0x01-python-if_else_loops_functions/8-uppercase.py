10fb455692d9edf96e2bc7b513e89a5b2794a9dc
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-05 17:24:43 +0100

    added task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def uppercase(str):
+    fix = ""
+    for char in str:
+        if 'a' <= char <= 'z':
+            char = chr(ord(char) - (ord('a') - ord('A')))
+        fix += char
+    print(fix, end="")
+    print("")
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
