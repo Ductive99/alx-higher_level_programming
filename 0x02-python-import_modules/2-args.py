b8ea83b9f03326e622e6b71aa656c3badfdca1be
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-07 19:28:56 +0100

    added task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv) - 1
+    count = 1
+    print("{:d} argument".format(length), end="")
+    if length == 1:
+        print(":")
+    elif length == 0:
+        print("s.")
+    else:
+        print("s:")
+    for argument in enumerate(argv[1:], 1):
+        print("{:d}: {}".format(count, argument))
+        
