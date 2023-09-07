d05fce76fb1ff3038a7c8d9b7fcbf07bdcf1824e
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-08 00:14:11 +0100

    advanced task 100 - debugging

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,14 +2,11 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
-
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
     a = int(argv[1])
     b = int(argv[3])
-
     match argv[2]:
         case '+':
             print(f"{a} + {b} = {add(a, b)}")
