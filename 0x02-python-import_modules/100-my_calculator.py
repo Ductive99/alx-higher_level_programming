245b3982055d23b3561f3340f09cf15586465e95
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-08 00:20:41 +0100

    advanced task 100 - more debugging

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    from sys import argv
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
