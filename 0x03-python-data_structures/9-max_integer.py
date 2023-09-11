3f151ab7aa8523b6b0ab5404fec94ad7563f8d7d
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-11 18:05:01 +0100

    task 9 fix - all negative list fix

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = 0
     if my_list is None or len(my_list) == 0:
         return None
+    max = my_list[0]
     for int in my_list:
         if int > max:
             max = int
