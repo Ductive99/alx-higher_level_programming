007213c85342a27d2440d4b3cc0083655c18727a
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-13 21:30:14 +0100

    New return implementation

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,4 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
-    return max([v for k, v in a_dictionary.items()])
+    return max(a_dictionary, key=a_dictionary.get)
