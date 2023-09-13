8e7d3d009973d4996457baffdfe582e78c0e0b32
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-13 21:50:00 +0100

    added advanced task 101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda y: list(lambda x: x ** 2, y)), matrix)
+    return list(map(lambda y: list(map(lambda x: x ** 2, y)), matrix))
