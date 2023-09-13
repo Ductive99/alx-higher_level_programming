8bc28292b72223e4cb3d0bbc3b3bedca9fe673d1
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-13 21:48:41 +0100

    advanced task 101 - 3 lines max

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    result = []
-
-    for row in matrix:
-        result.append(list(map(lambda x: x ** 2, row)))
-    
-    return result
+    return list(map(lambda y: list(lambda x: x ** 2, y)), matrix)
