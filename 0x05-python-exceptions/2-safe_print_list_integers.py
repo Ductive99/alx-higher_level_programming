ef174c15d84a5502c5e354925e99e728373f3bd3
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-25 17:40:33 +0100

    removed IndexError checking

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for item in my_list[:x]:
+    for i in range:
         try:
-            print("{:d}".format(item), end="")
+            print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
             pass
