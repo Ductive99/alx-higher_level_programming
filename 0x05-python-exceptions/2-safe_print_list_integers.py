2094f7f0eeeada16281fa58fa8bc32e252d66ba3
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-25 17:29:20 +0100

    added tasks 1 - 2 - 3

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for item in my_list[:x]:
+        try:
+            print("{:d}".format(item), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return count
