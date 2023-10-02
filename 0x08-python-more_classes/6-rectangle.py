14fc28bf069ff153ffd55983e1ee95dc7980c525
El Houssain Souhail <sou.ductive@gmail.com>
2023-10-02 19:13:18 +0100

    removed test cases

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -70,12 +70,3 @@ class Rectangle:
     def __del__(self):
         Rectangle.number_of_instances -= 0
         print("Bye rectangle...")
-
-
-my_rectangle_1 = Rectangle(2, 4)
-my_rectangle_2 = Rectangle(2, 4)
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
-del my_rectangle_1
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
-del my_rectangle_2
-print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
