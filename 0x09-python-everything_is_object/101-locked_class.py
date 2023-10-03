1b085e068d1d05e9bcb8202e00a0cd58c6f914c7
El Houssain Souhail <sou.ductive@gmail.com>
2023-10-03 18:10:17 +0100

    added documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
+"""Module that defines a LockedClass"""
 
 class LockedClass:
+    """LockedClass class"""
     def __setattr__(self, name, value):
         if name == "first_name":
             super().__setattr__(name, value)
