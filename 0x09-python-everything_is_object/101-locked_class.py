db9330f08cf5403e38219f9619698d8a025def14
El Houssain Souhail <sou.ductive@gmail.com>
2023-10-03 18:13:43 +0100

    fixed operation order

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Module that defines a LockedClass"""
 
+
 class LockedClass:
     """LockedClass class"""
     def __setattr__(self, name, value):
-        if name == "first_name":
-            super().__setattr__(name, value)
-        raise AttributeError(f"'LockedClass' object has not atribute '{name}'")
+        if name != "first_name":
+            err = f"'LockedClass' object has not atribute '{name}'"
+            raise AttributeError(err)
+        super().__setattr__(name, value)
