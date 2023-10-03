9530d0c8b343dbee18d10f981ac07d34db055f46
El Houssain Souhail <sou.ductive@gmail.com>
2023-10-03 17:51:54 +0100

    slight look wise improvement

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(n=[0]):
+def magic_string(n=[-1]):
     n[0] += 1
-    return "BestSchool" + ", BestSchool" * (n[0] - 1)
+    return "BestSchool" + ", BestSchool" * n[0]
