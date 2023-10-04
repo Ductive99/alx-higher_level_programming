4f85ebc5ea4ca1d3a7857c9d1d4d2a2c49e5be78
El Houssain Souhail <sou.ductive@gmail.com>
2023-10-04 20:46:49 +0100

    added task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,16 +7,15 @@ max_integer = __import__("6-max_integer").max_integer
 class TestMax(unittest.TestCase):
     def test_positve(self):
         e1 = [12, 34, 43, 8, 9, 45]
-        self.asserEqual(max_integer(e1), 45)
+        self.assertEqual(max_integer(e1), 45)
     
     def test_negative(self):
         e1 = [-12, -34, -43, -8, -9, -45]
-        self.asserEqual(max_integer(e1), -8)
+        self.assertEqual(max_integer(e1), -8)
 
     def test_non_int(self):
         string = "Test"
-        with self.assertRaises(TypeError):
-            max_integer(string)
+        self.assertEqual(max_integer(string), 't')
 
 
 if __name__ == "__main__":
