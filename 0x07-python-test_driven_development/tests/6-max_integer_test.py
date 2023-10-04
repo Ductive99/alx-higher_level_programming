70a238cf2af1881a2dfb8756b8d68c77c07e5bdd
El Houssain Souhail <sou.ductive@gmail.com>
2023-10-04 20:16:59 +0100

    typo fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """max_integer() unit test"""
 import unittest
-max_integer = __import("6-max_integer").max_integer
+max_integer = __import__("6-max_integer").max_integer
 
 
 class TestMax(unittest.TestCase):
