9474c7c12722cb1131b5ee33d79c8cec12db1105
El Houssain Souhail <sou.ductive@gmail.com>
2023-10-17 16:55:49 +0100

    added instance creating method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,3 +41,10 @@ class Base:
         if json_string is None:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with attributes set"""
+        new = cls(1, 1)
+        new.update(**dictionary)
+        return new
