4d7daec3cf3f4c744ee1a5d00ad4fc6252fefb60
El Houssain Souhail <sou.ductive@gmail.com>
2024-01-08 21:33:13 +0100

    Fixed typo

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 let num = parseInt(process.argv[2]);
-if (isNaN(num) || process.argv[2] === undefined 
+if (isNaN(num) || process.argv[2] === undefined) {
   console.log('Missing number of occurences');
 } else {
   while (num > 0) {
