fae27765f309916e716e7fa858d54cba6990a272
El Houssain Souhail <sou.ductive@gmail.com>
2024-01-09 19:38:05 +0100

    Fixed spelling + execution rights

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Returns how many times an item appears in a list
 
-exports.Occurences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   return list.filter(item => item === searchElement).length;
 };
