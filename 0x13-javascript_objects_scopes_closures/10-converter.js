9c62158687a5dbbc96d930db894df1fdb3ee22ab
El Houssain Souhail <sou.ductive@gmail.com>
2024-01-09 19:39:07 +0100

    Fixed styling (semistandard)

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // Converts a number from base 10 to the given base as argument.
 
-
 exports.converter = function (base) {
   function myConverter (n) {
     return n.toString(base);
