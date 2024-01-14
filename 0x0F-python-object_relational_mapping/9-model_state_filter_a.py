4a80ce1938da8f746e6f023a0e1c6446817f4f0e
El Houssain Souhail <sou.ductive@gmail.com>
2024-01-14 21:27:29 +0100

    Added character finding script

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that lists all state objects
+from a database
+"""
+
+
+def main():
+    """
+    Connecting to the database and querying it
+    """
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    from sys import argv
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.contains('a'))
+
+    for state in states:
+        print("{0}: {1}".format(state.id, state.name))
+
+
+if __name__ == "__main__":
+    """ Main Entry Point """
+    main()
