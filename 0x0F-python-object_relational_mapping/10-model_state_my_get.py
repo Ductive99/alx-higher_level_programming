8c54a98f87bdfc16468996809e8869b75d2813dd
El Houssain Souhail <sou.ductive@gmail.com>
2024-01-14 21:30:58 +0100

    Added city search script

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script that lists the  state object
+that matches the passed name
+from a database
+"""
+
+
+def main():
+    """
+    Connecting to the database and querying it
+    """
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    from sys import argv
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first = session.query(State).filter(State.name == argv[4]).first()
+
+    if first is None:
+        print("Not found")
+    else:
+        print("{0}: {1}".format(first.id, first.name))
+
+
+if __name__ == "__main__":
+    """ Main Entry Point """
+    main()
