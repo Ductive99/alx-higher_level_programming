4c9467b8646f26121d0a5d582136b954c1fd0e1f
El Houssain Souhail <sou.ductive@gmail.com>
2024-01-16 19:01:28 +0100

    Added Instance Deletion Script

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Script that deletes all State objects
+with a name containing the letter a
+from the hbtn_0e_6_usa database
+"""
+
+
+def main():
+    """
+    Connecting to the database and querying it
+    """
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    from sys import argv
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.contains('a')).all()
+
+    for state in states:
+        session.delete(state)
+        session.commit()
+
+
+if __name__ == "__main__":
+    """ Main Entry Point """
+    main()
