c22135ed9c55754b9157f91256179dc0d0b750eb
El Houssain Souhail <sou.ductive@gmail.com>
2024-01-27 00:09:47 +0100

    Changed implementation

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL - Displays all HTTP methods the server will accept
-curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d ' ' -f2
+curl -sI "$1" | grep "Allow:" | sed -ne 's/^Allow: //p'
