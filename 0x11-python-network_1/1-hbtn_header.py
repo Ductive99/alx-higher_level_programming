26ddb53897467e82017b3e1cc98befca4fbc8498
El Houssain Souhail <sou.ductive@gmail.com>
2024-01-28 21:13:46 +0100

    Refactor for improved execution flow

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,12 +3,11 @@
 - Takes in a URL + Sends a request to it
 - Displays the value of the X-Request-Id variable
 """
-import urllib.request as request
-import sys
+if __name__ == "__main__":
+    import urllib.request as request
+    import sys
 
-url = sys.argv[1]
+    url_req = request.Request(sys.argv[1])
 
-url_req = request.Request(url)
-
-with request.urlopen(url_req) as response:
-    print(response.headers.get('X-Request-Id'))
+    with request.urlopen(url_req) as response:
+        print(response.headers.get('X-Request-Id'))
