7716f95c3b1c99363c8877c47030a1f57134b09b
El Houssain Souhail <sou.ductive@gmail.com>
2024-01-30 18:28:04 +0100

    Added advanced POST request script

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that takes in a letter, and
+Sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
+"""
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    data = {"q": letter}
+
+    r = requests.post(url, data=data)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
