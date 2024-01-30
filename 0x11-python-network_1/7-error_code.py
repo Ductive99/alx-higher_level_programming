a620f81adbf9afce61d2733d033214b72cd2a4f8
El Houssain Souhail <sou.ductive@gmail.com>
2024-01-30 18:12:15 +0100

    Added script to send a request to a URL

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+- Takes in a URL and sends a request to it
+- Display the body of the response
+- Using the request module
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    response = requests.get(sys.argv[1])
+    if response.status_code < 400:
+        print(response.text)
+    else:
+        print(f"Error code: {r.status_code}")
