9299a18dbcc8ba531b75b8a2a28997ed87a50470
El Houssain Souhail <sou.ductive@gmail.com>
2024-01-30 17:14:10 +0100

    Improved styling

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     import requests
 
     response = requests.get("https://alx-intranet.hbtn.io/status")
-    
     print("Body response:")
     print(f"\t- type: {type(response.text)}")
     print(f"\t- content: {response.text}")
